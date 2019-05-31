a2650615af81f7da4f2d3703dd31da115ed5b0e8
Theodore Byron <ted@tedbyron.com>
2019-05-31 18:01:44 -0400

    Add social media icons; update scripts

--- httpdocs/assets/js/min/scripts.js
+++ httpdocs/assets/js/min/scripts.js
@@ -1,1 +1,1 @@
-"use strict";$(".mobile-menu").on("click",function(){$(".mobile-dropdown").slideToggle("fast")});
+"use strict";$(".mobile-menu").on("click",function(){$(".mobile-dropdown").slideToggle("fast")}),Array.prototype.forEach.call(document.forms,function(a){a.querySelector('input[type="submit"]').addEventListener("click",function(){a.querySelectorAll(":required").forEach(function(a){a.checkValidity()||a.classList.add("invalid-input"),a.addEventListener("input",function(){a.checkValidity()?a.classList.remove("invalid-input"):a.classList.add("invalid-input")})})})});
